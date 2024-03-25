55bba20cff027040bbe244378c1e9c2f31c8ebb3
Miss-Sonia <soniaihemeo@gmail.com>
2024-03-25 17:49:19 +0100

    complete

---
+++ Score Board/src/components/InternData.jsx
@@ -0,0 +1,304 @@
+
+import img1 from '../assets/img 1.jpg'
+import img2 from '../assets/img 2.jpg'
+import img3 from '../assets/img 3.jpg'
+import img4 from '../assets/img 4.jpg'
+import img5 from '../assets/img 5.jpg'
+import img6 from '../assets/img 6.jpg'
+import img7 from '../assets/img 7.jpg'
+import img8 from '../assets/img 8.jpg'
+import img9 from '../assets/img 9.jpg'
+import img10 from '../assets/img 10.jpg'
+
+// const totalscores = 
+// Backend.tasks.task1 +
+// Backend.tasks.task
+
+export const Backend = [
+
+    {
+        img:img1,
+        id: 1,
+        name: 'Promise Adams ',
+        path: 'BackEnd Development' ,
+        tasks:  {
+            task1: 10,
+            task2: 30,
+            task3: 20,
+            task4: 14,
+            task5: 50,
+        },
+        totalscore : 301,
+    },
+    {
+        img:img2,
+        id : 2,
+        name: 'Obinna Charles',
+        path: 'BackEnd Development' ,
+        tasks:  {
+            task1: 30,
+            task2: 39,
+            task3: 42,
+            task4: 25,
+            task5: 44,
+        },
+        totalscore : 340
+    },
+    {
+        img:img3,
+        id : 3,
+        name: 'Ike Ikechukwu',
+        path: 'BackEnd Development' ,
+        tasks:  {
+            task1: 34,
+            task2: 24,
+            task3: 15,
+            task4: 42,
+            task5: 30,
+        },
+        totalscore : 200
+    },
+    {
+        img:img4,
+        id : 4,
+        name: 'Samuel Davies',
+        path: 'BackEnd Development' ,
+        tasks:  {
+            task1: 30,
+            task2: 20,
+            task3: 40,
+            task4: 40,
+            task5: 50,
+        },
+        totalscore : 450
+    },
+    {
+        img:img5,
+        id : 5,
+        name: 'Chidinma Ike',
+        path: 'BackEnd Development' ,
+        tasks:  {
+            task1: 23,
+            task2: 42,
+            task3: 13,
+            task4: 21,
+            task5: 32,
+        },
+        totalscore : 334
+    },
+    {
+        img:img6,
+        id : 6,
+        name: 'Juliet Okonkwo',
+        path: 'BackEnd Development' ,
+        tasks:  {
+            task1: 23,
+            task2: 34,
+            task3: 43,
+            task4: 24,
+            task5: 39,
+        },
+        totalscore : 231
+    },
+    {
+        img:img7,
+        id: 7,
+        name: 'Ikenna Mbah',
+        path: 'BackEnd Development' ,
+        tasks:  {
+            task1: 34,
+            task2: 50,
+            task3: 14,
+            task4: 23,
+            task5: 43,
+        },
+        totalscore : 432
+    },
+    {
+        img:img8,
+        id : 8,
+        name: 'Juliet Sunday',
+        path: 'BackEnd Development' ,
+        tasks:  {
+            task1: 30,
+            task2: 50,
+            task3: 50,
+            task4: 40,
+            task5: 15,
+        },
+        totalscore : 422
+    },
+    {
+        img:img9,
+        id : 9,
+        name: 'John Mbah',
+        path: 'BackEnd Development' ,
+        tasks:  {
+            task1: 42,
+            task2: 25,
+            task3: 43,
+            task4: 20,
+            task5: 19,
+        },
+        totalscore : 134
+    },
+    {
+        img:img10,
+        id : 10,
+        name: 'Eniola Williams',
+        path: 'BackEnd Development' ,
+        tasks:  {
+            task1: 30,
+            task2: 39,
+            task3: 19,
+            task4: 46,
+            task5: 50,
+        },
+        totalscore : 532
+    },
+
+]
+export const Frontend = [
+
+    {
+        img:img1,
+        id: 1,
+        name: 'Sonia Iheme',
+        path: 'Frontend development' ,
+        tasks:  {
+            task1: 30,
+            task2: 30,
+            task3: 30,
+            task4: 30,
+            task5: 30,
+        },
+        totalscore : 323
+    },
+    {
+        img:img2,
+        id : 2,
+        name: 'Demola Grey',
+        path: 'Frontend development' ,
+        tasks:  {
+            task1: 30,
+            task2: 30,
+            task3: 30,
+            task4: 30,
+            task5: 30,
+        },
+        totalscore : 233
+    },
+    {
+        img:img3,
+        id : 3,
+        name: 'Kosi Uche',
+        path: 'Frontend development' ,
+        tasks:  {
+            task1: 30,
+            task2: 30,
+            task3: 30,
+            task4: 30,
+            task5: 30,
+        },
+        totalscore : 342
+    },
+    {
+        img:img4,
+        id : 4,
+        name: 'Alex Ani',
+        path: 'Frontend development' ,
+        tasks:  {
+            task1: 30,
+            task2: 30,
+            task3: 30,
+            task4: 30,
+            task5: 30,
+        },
+        totalscore : 432
+    },
+    {
+        img:img5,
+        id : 5,
+        name: 'Samuel Okoro',
+        path: 'Frontend development' ,
+        tasks:  {
+            task1: 30,
+            task2: 30,
+            task3: 30,
+            task4: 30,
+            task5: 30,
+        },
+        totalscore : 301
+    },
+    {
+        img:img6,
+        id : 6,
+        name: 'sally Thomas ',
+        path: 'Frontend development' ,
+        tasks:  {
+            task1: 30,
+            task2: 30,
+            task3: 30,
+            task4: 30,
+            task5: 30,
+        },
+        totalscore : 298
+    },
+    {
+        img:img7,
+        id: 7,
+        name: 'Chioma Eze',
+        path: 'Frontend development' ,
+        tasks:  {
+            task1: 30,
+            task2: 30,
+            task3: 30,
+            task4: 30,
+            task5: 30,
+        },
+        totalscore :290
+    },
+    {
+        img:img8,
+        id : 8,
+        name: 'Precious Agbo',
+        path: 'Frontend development' ,
+        tasks:  {
+            task1: 30,
+            task2: 30,
+            task3: 30,
+            task4: 30,
+            task5: 30,
+        },
+        totalscore: 392
+    },
+    {
+        img:img9,
+        id : 9,
+        name: 'Charles Okeke',
+        path: 'Frontend development' ,
+        tasks:  {
+            task1: 30,
+            task2: 30,
+            task3: 30,
+            task4: 30,
+            task5: 30,
+        },
+        totalscore : 490
+    },
+    {
+        img:img10,
+        id : 10,
+        name: 'Eli Osondu',
+        path: 'Frontend development' ,
+        tasks:  {
+            task1: 30,
+            task2: 30,
+            task3: 30,
+            task4: 30,
+            task5: 30,
+        },
+        totalscore : 311
+    },
+
+]
